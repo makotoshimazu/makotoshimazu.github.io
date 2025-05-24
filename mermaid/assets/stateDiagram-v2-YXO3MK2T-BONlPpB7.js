1ca33718ca98dd838c86e4fbebb99ac89550a8b4
Makoto Shimazu <makoto.shimaz@gmail.com>
2025-05-25 07:00:49 +0900

    update mermaid

---
+++ mermaid/assets/stateDiagram-v2-YXO3MK2T-BONlPpB7.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-AEK57VVT-DsfWpG-f.js";import{_ as i}from"./mermaid-BdmZvM0I.js";import"./chunk-RZ5BOZE2-DNqD08YN.js";import"./monaco-editor-B3cPCkWu.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
